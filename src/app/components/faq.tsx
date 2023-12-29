f71e19ff7ff455d114a41d7b4b2f079f491aea7e
NikolaBogicevic <nikola.bogicevic1984@gmail.com>
2023-12-29 10:11:52 +0100

    [WIP]

--- src/app/components/faq.tsx
+++ src/app/components/faq.tsx
@@ -1,22 +1,26 @@
 "use client";
-import React, { useState } from "react"
-import Image, { StaticImageData } from "next/image"
-import expand from "../expand.png"
-import styles from '../page.module.css'
+import React, { useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import expand from "../expand.png";
+import styles from "../page.module.css";
 
 export type FaqProps = {
-    title: string,
-    body: string,
-}
+  title: string;
+  body: string;
+};
 
-export function Faq({title,body}:FaqProps) {
+export function Faq({ title, body }: FaqProps) {
   const [showLanguages, setShowLanguages] = useState(false);
-    return (
-        <div className={styles.cardQuestion}>
-            <p onClick={() => setShowLanguages((curr) => !curr)}><h3>{title} <Image src={expand} alt={""} width={16}/></h3></p>
-            { showLanguages && <p>{body}</p>}
-        </div>
-    )
+  return (
+    <div className={styles.cardQuestion}>
+      <p onClick={() => setShowLanguages((curr) => !curr)}>
+        <h3>
+          {title} <Image src={expand} alt={""} width={16} />
+        </h3>
+      </p>
+      {showLanguages && <p>{body}</p>}
+    </div>
+  );
 }
 
-export default Faq
+export default Faq;
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,41 +1,44 @@
 "use client";
-import React, { useState } from "react"
-import Image, { StaticImageData } from "next/image"
-import vector from "../vector.png"
-import expand from "../expand.png"
-import styles from '../page.module.css'
+import React, { useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import vector from "../vector.png";
+import expand from "../expand.png";
+import styles from "../page.module.css";
 
 export default function Navbar() {
   const [showLanguages, setShowLanguages] = useState(false);
-    return (
+  return (
     <nav className={styles.navigation}>
       <div className={styles.navigationIcon}>
         <Image src={vector} alt={""} />
-      <p>Finanxe</p>
+        <p>Finanxe</p>
       </div>
-        <ul className={styles.navigationBar}>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">About Us</a>
-          </li>
-          <li>
-            <a href="">Contact Us</a>
-          </li>
-          <li>
-            <p onClick={() => setShowLanguages((curr) => !curr)}>Language <Image src={expand} alt={""} width={10}/></p>
-        { showLanguages &&   <ul className={styles.language}>
+      <ul className={styles.navigationBar}>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About Us</a>
+        </li>
+        <li>
+          <a href="">Contact Us</a>
+        </li>
+        <li>
+          <p onClick={() => setShowLanguages((curr) => !curr)}>
+            Language <Image src={expand} alt={""} width={10} />
+          </p>
+          {showLanguages && (
+            <ul className={styles.language}>
               <li>
                 <a href="">English 🇬🇧</a>
               </li>
               <li>
                 <a href="">Serbian</a>
               </li>
-            </ul>}
-          </li>
-        </ul>
+            </ul>
+          )}
+        </li>
+      </ul>
     </nav>
-
-    )
+  );
 }
