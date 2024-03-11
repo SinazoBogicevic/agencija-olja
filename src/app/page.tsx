016bf4faff6f4aa2bff3fb876335d17f9ab820a5
NikolaBogicevic <nikola.bogicevic1984@gmail.com>
2024-03-11 10:49:23 +0100

    [WIP]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Card from "./components/card";
+import Companies from "./components/companies";
 import Contact from "./components/contact";
-import Faq from "./components/faq";
 import Faqs from "./components/faqs";
 import Footer from "./components/footer";
 import { Hero } from "./components/hero";
 import Navbar from "./components/navbar";
-import Section from "./components/section";
-import styles from "./page.module.css";
-import { questions, services } from "./statics";
-import Companies from "./components/companies";
-import Testimonials from "./components/testimonials";
 import Packages from "./components/packages";
+import Section from "./components/section";
+import Testimonials from "./components/testimonials";
+import styles from "./page.module.css";
+import { services } from "./statics";
 
 export default function Home() {
   return (
